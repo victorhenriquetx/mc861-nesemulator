70b916e82c12cfec8b03c657fedd02a5d2e42f77
Willian <will.hayashida07@gmail.com>
2019-09-06 21:55:37 -0300

    creating makefile and organizing repo to realize future tests

--- month_2_processor/processor.py
+++ month_2_processor/processor.py
@@ -1,25 +1,31 @@
 import os
-from . import Switcher as s
+import src.Switcher as s
 
 
 def emula():
+    # carrega arquivo em bytes p memo
     memo = readFile()
     pc = init_pos
 
     while True:
         instruction = read_memo(pc)
-        decod_instruction = decodeInstruction(instruction)
+        decod_instruction = decodeInstruction(instruction) # reconverter p strings?
 
         a = s.Switcher()
-        err = a.which_instruction(decod_instruction)
+        err = a.which_instruction(decod_instruction) # tratativa da instrucao no metodo de classe
+
+        log(err)
+
+#TODO implementar funcoes auxiliares
 
 
-
-def readFile():
-    return ""
+def readFile(filename):
+    return open(filename, "rb").read()  
 
 def decodeInstruction():
     return ""
 
+def log():
+    return ""
 
 emula()
