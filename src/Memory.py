030907d5e10103071c6d8061a33df066da0c47d4
Rogério Meirelles <rogerio@brainweb.com.br>
2019-10-02 19:46:01 -0300

    Fix push_stack function

--- src/Memory.py
+++ src/Memory.py
@@ -31,7 +31,7 @@ class Memory():
         return self.mem[self.stack_offset + stack_register.value]
 
     def push_stack(self, stack_register, value):
-        self.mem[self.stack_offset + stack_register] = value
+        self.mem[self.stack_offset + stack_register.value] = value
         stack_register.value += 1
     
     def write_memo(self, position, value):
